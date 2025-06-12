1735294d4e8f74305a7a540f86f045f075e3ed36
Mo-Cafe <129607778+Mo-Cafe@users.noreply.github.com>
2025-06-13 00:15:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request, url_for
 from gtts import gTTS
 import os
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # 모든 출처 허용 (개발용)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -21,3 +23,5 @@ def index():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Render가 자동으로 PORT를 지정해줌
     app.run(host='0.0.0.0', port=port)
+
+
