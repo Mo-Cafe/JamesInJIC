d59320b9a2313e72fb460d609980f3cb6deec3ec
Mo-Cafe <129607778+Mo-Cafe@users.noreply.github.com>
2025-06-12 20:22:58 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ def index():
     return render_template('index.html', audio_file=audio_file)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render가 자동으로 PORT를 지정해줌
+    app.run(host='0.0.0.0', port=port)
